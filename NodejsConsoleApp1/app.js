d8efc72cde7f4bcc13701fa96f6de1ac500fcafa
Shahrul <shahrul.izzam@gmail.com>
2018-07-15 22:43:56 +0800

    Changed html input.

--- NodejsConsoleApp1/app.js
+++ NodejsConsoleApp1/app.js
@@ -1,45 +1,3 @@
-// new jsdom
-
-//const jsdom = require("jsdom");
-//const { JSDOM } = jsdom;
-
-//const options = {
-//    resources: "usable",
-//    runScripts: "dangerously"
-//};
-//JSDOM.fromURL("http://localhost:9000/", options).then(dom => {
-//    console.log(dom.window);
-//});
-//// jQuery.
-//var $ = require("jquery")(dom.window);
-
-//global.window = dom.window;
-//global.document = dom.window.document;
-//Object.keys(document.defaultView).forEach((property) => {
-//    if (typeof global[property] === 'undefined') {
-//        global[property] = document.defaultView[property];
-//    }
-//});
-
-//global.navigator = {
-//    userAgent: 'node.js'
-//};
-
-//global.URL = {
-//    createObjectURL: function (input) { return input; }
-//};
-
-//var tiny = require("tinymce/tinymce");
-//console.log(tiny);
-
-//var tinyMCE = dom.window.tinyMCE;
-
-//tinyMCE.init({
-//    selector: "textarea",
-//    theme: false
-//});
-
-
 // old jsdom
 //var jsdom = require("jsdom/lib/old-api.js");
 //var fs = require("fs");
@@ -87,7 +45,7 @@ var roundTrip = function (html) {
     });
 }
 
-roundTrip('<p><span style="font - family: Verdana; font - size: 13.3333px;">Also, we will have many customers installing and running the same software. So if the product goes out with bugs, we will have to handle a large number of support requests and issue a large amount of patches. Different customers will be running our software in different environments (different databases, different versions of office on the server etc.) so, although we do our best to test on all possible combinations, we must make sure the design is not a &lsquo;brittle&rsquo; one which is dependent on a specific operating environment, but one that is standards-based and likely to function correctly even in environments we hadn&rsquo;t foreseen.</span></p>Also, we will have many customers installing and running the same software. So if the product goes out with bugs, we will have to handle a large number of support requests and issue a large amount of patches. Different customers will be running our software in different environments (different databases, different versions of office on the server etc.) so, although we do our best to test on all possible combinations, we must make sure the design is not a ‘brittle’ one which is dependent on a specific operating environment, but one that is standards-based and likely to function correctly even in environments we hadn’t foreseen.');
+roundTrip("Tell the world that we finally get it alright.");
 
 console.log('Press any key to exit');
 process.stdin.setRawMode(true);
